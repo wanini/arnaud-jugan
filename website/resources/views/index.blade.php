8cdd37fe199bc279a63f1a016df28074301a3030
Antoine Bachelier <antoine.bachelier95@gmail.com>
2016-01-26 18:58:12 +0100

    intégration de l'index

---
+++ website/resources/views/index.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Arnaud Jugan</title>
+        <meta charset="utf-8">
+        <meta description="Guide traversée de la baie du Mont Saint Michou">
+        <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+        <link rel="stylesheet" href="<?= asset('css/style.css') ?>">
+    </head>
+    <body>
+        <nav>
+            MENU
+        </nav>
+        <section id="main">
+                <img src="<?= asset('img/logo-blanc.png') ?>" alt="logo">
+                <h1>Arnaud Jugan</h1>
+                <h2>Guide de la Baie du Mont Saint Michel</h2>
+            <a href="#">Qui suis-je ?<br><i></i></a>
+        </section>
+    </body>
+</html>
