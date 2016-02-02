51a4cbcd10c1206e4cb2c461d9f19f4d2aa5dbeb
Antoine Bachelier <antoine.bachelier95@gmail.com>
2016-02-02 18:20:46 +0100

    index presque terminé

--- website/resources/views/index.blade.php
+++ website/resources/views/index.blade.php
@@ -6,17 +6,27 @@
         <meta description="Guide traversée de la baie du Mont Saint Michou">
         <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
-        <link rel="stylesheet" href="<?= asset('css/style.css') ?>">
+        <link href='https://fonts.googleapis.com/css?family=Open+Sans:400,300,600,700,400italic|Lato:400,300,400italic,700|Slabo+27px' rel='stylesheet' type='text/css'>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+        <link rel="stylesheet" href="<?php echo asset('css/style.css'); ?>">
     </head>
     <body>
         <nav>
-            MENU
+            <div id="nav-icon">
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+                <p>MENU</p>
+            </div>
         </nav>
         <section id="main">
-                <img src="<?= asset('img/logo-blanc.png') ?>" alt="logo">
+                <img src="<?php echo asset('img/logo-blanc.png'); ?>" alt="logo">
                 <h1>Arnaud Jugan</h1>
                 <h2>Guide de la Baie du Mont Saint Michel</h2>
-            <a href="#">Qui suis-je ?<br><i></i></a>
+            <a href="#">Qui suis-je ?<br><i class="fa fa-angle-down"></i></a>
         </section>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
+        <script src="<?php echo asset('js/main.js'); ?>"></script>
     </body>
 </html>
